e054b1b6067076167a96361b2fec3199ce8a5bdc
khalid0316 <khalid.mowafi@gmail.com>
2021-05-24 18:27:11 -0400

    2nd commit, added second class

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -3,5 +3,6 @@ public class First {
     /*
             This is the First class
             Will publish after
+            new line in the second push
     */
 }
---
+++ src/test/java/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+
+    /*
+    *   2nd file, some new work
+    *   added line from second line
+    * */
+}
